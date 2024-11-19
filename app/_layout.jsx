bd30e228955868d9d6ac2bc3c9e867899c8234f0
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-11-19 11:20:07 +0100

    setup

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,21 +1,12 @@
-import { StyleSheet, View, Text } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import { Stack } from 'expo-router'
 
 
-export default function App(){
-    return (
-        <View style={styles.container}>
-            <Text>Open un app</Text>
-            <StatusBar style="auto"/>
-        </View>
-    );
+const RootLayout = () => {
+  return (
+    <Stack>
+        <Stack.Screen name='index' options={{headerShown: false}}/>
+    </Stack>
+    )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-})
+export default RootLayout
